33c64ed863000d1b28f255ebde668ba407d76f49
jad ibrahem <83648786+jadibrahem5@users.noreply.github.com>
2024-03-12 17:54:25 +0300

    Update urls.py

--- translate/quotation/urls.py
+++ translate/quotation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import TranslationRequestAPI, hello_view 
 urlpatterns = [
-     path('', hello_view, name='hello'),
-     path('translation-request/', TranslationRequestAPI.as_view(), name='translation_request_api'),
+     path('hello/', hello_view, name='hello'),
+     path('', TranslationRequestAPI.as_view(), name='translation_request_api'),
 ]
